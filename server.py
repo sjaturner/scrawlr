f7b51a995d43b2046389cafba7f00752ad50058d
simon turner <sjaturner@googlemail.com>
2012-06-30 14:21:38 +0100

    minimal server using webob

--- server.py
+++ server.py
@@ -5,8 +5,8 @@ def app(environ, start_response):
     req = Request(environ)
     pprint(req)
     if req.path_info=='/' and req.method=='GET':
-        start_response("200 OK", [("Content-type", "text/plain")])
-        return ["Hello World!",]
+        resp = Response("Hello World!", "200 OK", [ ("Content-type", "text/plain"), ])
+        return resp(environ, start_response)
     else:
         resp = Response()
         return resp(environ, start_response)
