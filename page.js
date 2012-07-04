52c6c829c73c0cea1af1f57210a6ba9bc1c6ff03
simon turner <sjaturner@googlemail.com>
2012-07-04 22:02:19 +0100

    more towards drag and select

--- page.js
+++ page.js
@@ -57,27 +57,42 @@ $(document).ready(function(){
    }
 
    function pen(){
-      var tool=this; 
-      tool.started=false;
+      var that=this; 
 
-      tool.mousedown=function(ev){
+      that.mousedown=function(ev){
+         var date=new Date();
+
          ctx.beginPath();
          ctx.moveTo(ev._x, ev._y);
-         tool.started=true;
+
+         that.down={}
+         that.down['time']=date.getTime();
+         that.down['x']=ev._x;
+         that.down['y']=ev._y;
       };
 
-      tool.mousemove=function(ev){
-         if(tool.started){
+      that.mousemove=function(ev){
+         if('down' in that){
             ctx.lineTo(ev._x, ev._y);
             ctx.strokeStyle = "#000000";
             ctx.stroke();
+
+            if(that['down'].time){
+               var date=new Date();
+               var time=date.getTime();
+               var dwell=time-that['down'].time;
+
+               that['down'].time=0;
+            }
+            else{
+            }
          }
       };
 
-      tool.mouseup=function(ev){
-         if(tool.started){
-            tool.mousemove(ev);
-            tool.started=false;
+      that.mouseup=function(ev){
+         if('down' in that){
+            that.mousemove(ev);
+            delete that['down'];
          }
       };
    }
